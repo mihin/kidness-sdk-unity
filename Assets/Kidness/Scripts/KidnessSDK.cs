9fc5785fb74cb9f253f8c6dfd51377f00c86f179
Mikhail Prisheltsev <mikhail.prisheltsev@datcroft.com>
2017-02-08 13:58:30 +0300

    init files

---
+++ Assets/Kidness/Scripts/KidnessSDK.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidnessSDK : MonoBehaviour {
+
+	void Start () {
+		//AndroidNative.TwitterInit();
+	}
+	
+	void Update () {
+		
+	}
+}
