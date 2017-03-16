dd55741ad2e1e123b18e199ead2409e00aee38ff
Mikhail Prisheltsev <mikhail.prisheltsev@datcroft.com>
2017-03-16 15:07:31 +0300

    api test request

--- Assets/Samekids/Scripts/SamekidsAPI.cs
+++ Assets/Samekids/Scripts/SamekidsAPI.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define TEST_MODE
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -8,8 +10,11 @@ namespace Samekids
 {
     public class SamekidsAPI : MonoBehaviour
     {
-        //private const string SERVER = "samekids.com";
-        private const string SERVER = "78.24.219.173";
+#if TEST_MODE
+        private const string SERVER = "http://78.24.219.173";
+#else
+        private const string SERVER = "samekids.com";
+#endif
         private const string ADS_API = SERVER + "/api/showAds/";
 
         void Start()
@@ -19,12 +24,16 @@ namespace Samekids
 
         public void CheckAvalibleAds(Action<bool, string> callback, string android_id, string google_aid)
         {
+            //Debug.Log("SamekidsAPI :: CheckAvalibleAds. android_id=" + android_id + ", google_aid=" + google_aid);
+
             Dictionary<string, object> parms = new Dictionary<string, object>();
             //string userInfo = Json.Serialize(new Dictionary<string, string>() {{"google_aid", google_aid}, {"android_id", android_id} });
             //string userInfo = "{" + string.Format("google_aid: {0}, android_id: {1}", google_aid, android_id) + "}";
             parms.Add("user", new Dictionary<string, string>() { { "google_aid", google_aid }, { "android_id", android_id } });
             parms.Add("application", Application.bundleIdentifier);
-
+#if TEST_MODE
+            parms.Add("key", "test4test");
+#endif
             byte[] postData = Encoding.ASCII.GetBytes(Json.Serialize(parms));
 
             /*
@@ -34,10 +43,9 @@ namespace Samekids
                   android_id: android_id,
               },
             application: app_pack
+            'key' => 'test4test'
             }
-            */
 
-            /*
             успешный
             {
               success: true
@@ -51,7 +59,7 @@ namespace Samekids
               error_message: string
             }
             */
-            Action <bool, string> callbackInterpreter = (success, data) =>
+            Action<bool, string> callbackInterpreter = (success, data) =>
             {
                 if (success)
                 {
@@ -91,7 +99,15 @@ namespace Samekids
                 }
             };
 
-            SendPost(ADS_API, postData, callbackInterpreter);
+            SendServerRequest(ADS_API, postData, callbackInterpreter);
+        }
+
+        private void SendServerRequest(string url, byte[] postData, Action<bool, string> callback)
+        {
+            if (postData != null)
+                SendPost(url, postData, callback);
+            else
+                SendGet(url, callback);
         }
 
         private void SendPost(string url, Dictionary<string, string> parms, Action<bool, string> callback)
@@ -109,7 +125,14 @@ namespace Samekids
         private void SendPost(string url, byte[] postData, Action<bool, string> callback)
         {
             WWW www = new WWW(url, postData);
-            Debug.Log("SamekidsAPI :: SendPost. url=" + url + ", data="+ postData);
+            Debug.Log("SamekidsAPI :: SendPost. url=" + url + ", data="+ Encoding.ASCII.GetString(postData));
+            StartCoroutine(WaitForRequest(www, callback));
+        }
+
+        private void SendGet(string url, Action<bool, string> callback)
+        {
+            WWW www = new WWW(url);
+            Debug.Log("SamekidsAPI :: SendGet. url=" + url);
             StartCoroutine(WaitForRequest(www, callback));
         }
 
